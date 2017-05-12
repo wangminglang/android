ae4ccb639bcdd235c2bc0d01032cdc0b1d8cd3a4
wangminglang <minglang.wang@hjwang.com>
2017-05-12 15:11:23 +0800

    文件结构修改

--- app/common/NHNetUtil.js
+++ app/common/NHNetUtil.js
@@ -1,40 +1,38 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import Common from './constants';
 import md5 from "react-native-md5";
 
-import {
-} from 'react-native';
+export default Util = {
+	get: (url, successCallback, failCallback) => {
+		fetch(url)
+			.then((response) => response.text())
+			.then((responseText) => {
+				successCallback(JSON.parse(responseText));
+			})
+			.catch((error) => {
+				failCallback(err);
+			})
+	},
 
-// MD5 = require("crypto-js/md5");
-import CONFIG from './NHNetConfig.js';
-
-class NHNetUitl extends React.Component{
-    static POST(url,params,callback){
-
-        var tokenString = CONFIG.AppId;
-        tokenString = tokenString+CONFIG.AppKey;
-        var now = Date.now().toString().slice(0,10);
+	post: (url,params,callback) => {
+        let tokenString = Common.netConfig.AppId;
+        tokenString = tokenString+Common.netConfig.AppKey;
+        let now = Date.now().toString().slice(0,10);
         tokenString += now;
-        var token = md5.hex_md5(tokenString);
-        var param = {
-          'appId':CONFIG.AppId,
+        let token = md5.hex_md5(tokenString);
+        let param = {
+          'appId':Component.AppId,
           'deviceId' : '95501CD5-EF3D-4690-8E45-29D6610C7B85',
-          'clientversion' : CONFIG.clientversion,
+          'clientversion' : Component.clientversion,
           'time' : now,
           'token' : token
         };
-        for (var key in params) {
+        for (let key in params) {
           if (params.hasOwnProperty(key)) {
             param[key] = params[key];
           }
         }
 
-        var queryString = toQueryString(param);
+        let queryString = toQueryString(param);
         fetch(url,
                 {
                       method: 'POST',
@@ -59,7 +57,7 @@ class NHNetUitl extends React.Component{
         }
 }
 
-function toQueryString(obj) {
+let toQueryString = (obj) => {
     return obj ? Object.keys(obj).sort().map(function (key) {
         var val = obj[key];
         if (Array.isArray(val)) {
@@ -71,5 +69,3 @@ function toQueryString(obj) {
         return encodeURIComponent(key) + '=' + encodeURIComponent(val);
     }).join('&') : '';
 }
-
-module.exports = NHNetUitl;
---
+++ app/common/globalConstants.js
@@ -0,0 +1,19 @@
+import { Dimensions, Platform, PixelRatio } from 'react-native';
+
+global.gScreen = {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+    navBarHeight: Platform.OS === 'ios' ? 64 : 50,
+    navBarPaddingTop: Platform.OS === 'ios' ? 20 : 0,
+    onePix: 1 / PixelRatio.get(),
+    isIOS: Platform.OS === 'ios'
+}
+
+global.gColors = {
+    theme: 'rgb(217, 51, 58)',
+    background: '#f5f5f5',
+    border: '#d5d5d5',
+    healthGreen: 'rgb(142, 213, 7)',
+    healthYellow: 'rgb(254, 210, 10)',
+    healthRed: 'rgb(251, 25, 8)'
+}
