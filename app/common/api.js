eb01f539f84dcfb0fff2619678088823579d139f
guolei <lei.guo@hjwang.com>
2017-05-23 16:50:16 +0800

    fix

--- app/common/api.js
+++ app/common/api.js
@@ -8,3 +8,9 @@ export const LOGIN = "buyerapi/user/login";
 //获取验证码
 export const GET_VERIFY_CODE = "buyerapi/user/getVerifyCode";
 
+//首页
+export const GetHomeAllClassFirstPageData = "buyerapi/home/getHomeAllClassFirstPageData";
+
+//获取首页更多数据接口
+export const GetHomeMoreData = "buyerapi/home/getHomeMoreData";
+
